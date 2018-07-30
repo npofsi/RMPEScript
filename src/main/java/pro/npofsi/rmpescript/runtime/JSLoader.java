b9970e24b41a6f3b8775e76a6a170d956423dd5c
npofsi <npofsi@outlook.com>
2018-07-30 19:44:03 +0800

    feat(JSLoader): 脚本线城池添加

--- src/main/java/pro/npofsi/rmpescript/runtime/JSLoader.java
+++ src/main/java/pro/npofsi/rmpescript/runtime/JSLoader.java
@@ -1,4 +1,19 @@
 package pro.npofsi.rmpescript.runtime;
 
-public class JSLoader {
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+public class JSLoader extends Thread {
+    private String script,srcName;
+    public JSLoader(String srcName,String script){
+        this.srcName=srcName;
+        this.script=script;
+    }
+
+    @Override
+    public void run(){
+        Context cx = Context.enter();
+        Scriptable scope = cx.initStandardObjects();
+        Object result = cx.evaluateString(scope, script, srcName, 1, null);
+    }
 }
--- src/main/java/pro/npofsi/rmpescript/runtime/ScriptManager.java
+++ src/main/java/pro/npofsi/rmpescript/runtime/ScriptManager.java
@@ -1,4 +1,19 @@
 package pro.npofsi.rmpescript.runtime;
 
+import java.lang.*;
+import java.util.Stack;
+
 public class ScriptManager {
+
+    private ScriptManager(){}
+    public static ScriptManager getInstance(){
+        return ClassHolder.sInstance;
+    }
+    private static class ClassHolder{
+        private static final ScriptManager sInstance = new ScriptManager();
+    }
+
+    Stack<Thread> runstack;
+    Stack<Integer> ids;
+
 }
