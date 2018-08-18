ac7b9ab449de6f5700e835672ad4066986935416
npofsi <npofsi@outlook.com>
2018-08-18 17:18:23 +0800

    docs(README.md): 修改README

--- src/main/java/pro/npofsi/rmpescript/broadcast/FMLEventHandler.java
+++ src/main/java/pro/npofsi/rmpescript/broadcast/FMLEventHandler.java
@@ -8,15 +8,23 @@ import java.util.TreeMap;
 
 public class FMLEventHandler {
 
+
+//    public FMLEventHandler(){}
+//    public static FMLEventHandler getInstance(){
+//        return ClassHolder.sInstance;
+//    }
+//    private static class ClassHolder{
+//        private static final FMLEventHandler sInstance = new FMLEventHandler();
+//    }
     //这个统一形式定义的接口导致不能使用rhino所提供的interface的简写形式，会导致rhino找不到相应的类型
     public interface FMLEventCallback{
         public void call(FMLEvent event);
     }
     private static Map<String,Stack<FMLEventCallback>> eventStore=new TreeMap<String,Stack<FMLEventCallback>>();
-    public void registerCallback(String name,FMLEventCallback callback){
+    public static void registerCallback(String name,FMLEventCallback callback){
         if(eventStore.get(name)==null) { eventStore.put(name,new Stack<FMLEventCallback>());eventStore.get(name).push(callback); }else{ eventStore.get(name).push(callback); }
     }
-    public boolean unregisterCallback(String name,FMLEventCallback callback){
+    public static boolean unregisterCallback(String name,FMLEventCallback callback){
         if(eventStore.get(name)!=null) { return eventStore.get(name).remove(callback); }return false;
     }
     public static void runCallBack(String eventName,FMLEvent event){
